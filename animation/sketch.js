a6c7b00bd267beca5fe1e2229902ff54b567ef82
asf127 <144639842+asf127@users.noreply.github.com>
2023-11-05 20:51:29 -0500

    Add files via upload

---
+++ animation/sketch.js
@@ -0,0 +1,50 @@
+function setup() {
+  createCanvas(600, 600);
+  
+  vX = 2;
+  vY = 2;
+
+  x = width/2;
+  y = height/2;
+  
+  pos = x;
+  
+  diameter = 50;
+  
+}
+
+function draw() {
+  background(50);
+  
+  fill(66, 183, 255);
+  circle(x, y, diameter);
+  //circle(abs(x-600), abs(y-600), diameter);
+  
+  fill(176, 79, 255);
+  circle(pos, y, diameter);
+  
+  fill(255, 240, 71);
+  circle(x, pos, diameter);
+  
+  fill(255, 150, 64);
+  circle(pos, pos, diameter);
+  
+  x += vX;
+  y += vY;
+  
+  pos -= vX;
+  
+
+  if (x + diameter > width)
+    vX = -vX;
+  else if (x - diameter < 0)
+    vX = -vX;
+  
+  if (y + diameter > height)
+    vY = -vY;
+  else if (y - diameter < 0)
+    vY = -vY;
+  
+  diameter = abs(x - 300) * 0.3;
+  
+}
