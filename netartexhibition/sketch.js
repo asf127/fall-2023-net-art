678da13d76e4fa0aa8632b8614400aa44cb23acc
asf127 <144639842+asf127@users.noreply.github.com>
2023-12-17 20:10:17 -0500

    Update sketch.js

--- netartexhibition/sketch.js
+++ netartexhibition/sketch.js
@@ -1,7 +1,30 @@
+var arr = [];
+
 function setup() {
-  createCanvas(400, 400);
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent("container");
 }
 
 function draw() {
-  background(220);
+  clear();
+  
+  noStroke();
+  
+  var coor = {x: mouseX, y: mouseY};
+  
+  arr.push(coor);
+  
+  if (arr.length > 35){
+    arr.splice(0, 1);
+  }
+  
+  for (var i = 0; i < arr.length; i++){
+    fill(225-i*5, 200-i, 150);
+    ellipse(arr[i].x,arr[i].y,i*0.5,i*0.5);
+  }
+
+}
+
+function windowResized() {
+  canvas = resizeCanvas(windowWidth, windowHeight);
 }
